4e117c14ed12bba0d1c9a6321f58f390f95a6334
Moughsolomon02 <zahemenmoughkaa@gmail.com>
2025-07-25 03:07:10 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,1 @@
-const express = require('express');
-const app = express();
-app.get('/', (req, res) => res.send('Backend is running'));
-app.listen(process.env.PORT || 5000, () => console.log('Server started'));
+// Express server entry
